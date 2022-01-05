c1d1195ec832243b93decb9574d068ca8e0c2b34
Hiroki Horiuchi <x19290@gmail.com>
2022-01-05 13:04:14 +0900

    refactor: stop abusing __init__.py

--- mk4u/popen/__init__.py
+++ mk4u/popen/__init__.py
@@ -1,13 +1,3 @@
-from subprocess import call, Popen
-
-
-def xcall(args, **kwargs):
-    kwargs[r'shell'] = isinstance(args, str)
-    return call(args, **kwargs)
-
-
-class XPopen(Popen):
-    def __init__(self, args, **kwargs):
-        kwargs[r'shell'] = isinstance(args, str)
-        super(XPopen, self).__init__(args, **kwargs)
-
+from .xcall import xcall
+from .xpopen import XPopen
+xcall, XPopen  # avoid "not used" warnings
